b4fb7e9f599bb870685773133c3ebceb42c1cb47
borjafontenla <borjafontenla@gmail.com>
2022-12-23 19:26:49 +0100

    avances css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 'use strict'
+
+
+$(document).ready(function(){
+    
+    $('body').on({ 'mousemove' : function( e ){
+        console.clear()
+        let clientX = e.originalEvent.clientX
+        let clientY = e.originalEvent.clientX 
+        $('#cursor').css({
+            'left' : clientX + 'px',
+            'top' : clientY + 'px'
+        })
+    }})
+
+
+})
